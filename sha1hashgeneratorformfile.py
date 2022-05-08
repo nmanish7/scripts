180665af85fc1639d0dfe03686ce0219c28139a4
Manish <manishdas019@gmail.com>
2022-05-08 22:05:18 +0530

    Update sha1hashgeneratorformfile.py
    Adding File Checks.

--- sha1hashgeneratorformfile.py
+++ sha1hashgeneratorformfile.py
@@ -1,8 +1,15 @@
 import hashlib
+from pathlib import Path
 from tqdm import tqdm
 
 
-FILENAME = "rockyou.txt"  # Give the Input File
+USERINPUTFILENAME = "/usr/share/wordlists/rockyou.txt"  # Give the Input File
+FILENAME = Path(USERINPUTFILENAME) if Path(
+    USERINPUTFILENAME).exists() else None
+if not FILENAME:
+    print(f"{USERINPUTFILENAME} is not exist.")
+    exit()
+
 ENCODING = "latin-1"  # Give the Encoding
 
 
@@ -11,13 +18,13 @@ def file_next_line(filename: str, encoding: str):
     with open(file=filename, encoding=encoding) as file:
 
         for inner_line in file:
-            yield bytes(inner_line.strip(), encoding)
+            yield bytes(inner_line, encoding)
 
 
 file_object = file_next_line(FILENAME, ENCODING)
 
 
-FILENAMEOUT = f"{FILENAME.rsplit('.', maxsplit=1)[0]}_sha1Hash.txt"
+FILENAMEOUT = f"{FILENAME.name.rsplit('.', maxsplit=1)[0]}_sha1Hash.txt"
 ENCODINGOUT = "utf-8"
 
 with open(file=FILENAMEOUT, mode="w", encoding=ENCODINGOUT) as file_write:
